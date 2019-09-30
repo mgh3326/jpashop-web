4b9b81f282d98874d3240067ac1b0ffceeb889eb
kwanghyunMoon <mgh3326@naver.com>
2019-09-30 11:31:11 +0900

    update fetch 전략, 활용 메소드

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -17,16 +19,31 @@ public class Order {
   @GeneratedValue
   @Column(name = "order_id")
   private Long id;
-  @ManyToOne
+  @ManyToOne(fetch = LAZY)
   @JoinColumn(name = "member_id")
   private Member member;
 
-  @OneToMany(mappedBy = "order")
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
   private List<OrderItem> orderItems = new ArrayList<>();
-  @OneToOne
+  @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
   @JoinColumn(name = "delivery_id")
   private Delivery delivery;
   private LocalDateTime orderDate; // 주문 시간
   @Enumerated(EnumType.STRING)
   private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+  public void setMember(Member member) {
+    this.member = member;
+    member.getOrders().add(this);
+  }
+
+  public void addOrderItem(OrderItem orderItem) {
+    this.orderItems.add(orderItem);
+    orderItem.setOrder(this);
+  }
+
+  public void setDelivery(Delivery delivery) {
+    this.delivery = delivery;
+    delivery.setOrder(this);
+  }
 }
