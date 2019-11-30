bf86e4faa1254a7c9472bfcc723be89aa24bb091
kwanghyunMoon <mgh3326@naver.com>
2019-11-30 14:26:04 +0900

    update 회원 수정

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,6 +43,32 @@ public class MemberApiController {
     return new UpdateMemberResponse(findMember.getId(), findMember.getName());
   }
 
+  @GetMapping("/api/v1/members")
+  public List<Member> membersV1() {
+    return memberService.findMembers();
+  }
+  /**
+   * 조회 V2: 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+   */
+  @GetMapping("/api/v2/members")
+  public Result membersV2() {
+    List<Member> findMembers = memberService.findMembers();
+//엔티티 -> DTO 변환
+    List<MemberDto> collect = findMembers.stream()
+            .map(m -> new MemberDto(m.getName()))
+            .collect(Collectors.toList());
+    return new Result(collect);
+  }
+  @Data
+  @AllArgsConstructor
+  class Result<T> {
+    private T data;
+  }
+  @Data
+  @AllArgsConstructor
+  class MemberDto {
+    private String name;
+  }
   @Data
   static class UpdateMemberRequest {
     private String name;
